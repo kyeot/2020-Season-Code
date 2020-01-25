f8467da06f760d5422ca6f0575c6a279cec025fb
dkloek <44987539+dkloek@users.noreply.github.com>
2020-01-25 10:49:52 -0500

    Spark Controller integration for the fake shooter motor

--- 2020 Season Robot/src/main/java/frc/robot/subsystems/ShooterSubSystem.java
+++ 2020 Season Robot/src/main/java/frc/robot/subsystems/ShooterSubSystem.java
@@ -12,18 +12,34 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import frc.robot.Constants.ShooterConstants;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
 public class ShooterSubSystem extends SubsystemBase {
 
-  VictorSPX mShooterMotor;
+  //VictorSPX mShooterMotor;
+  private CANSparkMax mShooterMotor;
 
   public ShooterSubSystem() {
 
-    mShooterMotor = new VictorSPX(ShooterConstants.kShooterMotorPort);
+   // mShooterMotor = new VictorSPX(ShooterConstants.kShooterMotorPort);
 
+    mShooterMotor = new CANSparkMax(ShooterConstants.kShooterMotorPort, MotorType.kBrushless);
+
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+   // mShooterMotor.restoreFactoryDefaults();
+    SmartDashboard.putString("SPark ","" + mShooterMotor.getOutputCurrent() );
   }
 
   public void SetShooterSpeed(double speed) {
-    mShooterMotor.set(ControlMode.PercentOutput, speed);
+    SmartDashboard.putString("SPark ","" + mShooterMotor.getOutputCurrent() );
+    mShooterMotor.set(speed);   
   }
 
   @Override
